ebfec818a3af9ec98008e46180e37763326bd12b
ELBRIKIFATIMA <134740249+ELBRIKIFATIMA@users.noreply.github.com>
2025-01-27 16:40:34 +0100

    section services

--- sections/Services.tsx
+++ sections/Services.tsx
@@ -1,51 +1,94 @@
-'use client'
-import React from "react";
-import ImageCard from "@/Components/ImageCard";
+'use client';
+import React,{useState, useTransition} from "react";
+import { FaPlaceOfWorship } from "react-icons/fa";
+import { MdNavigateNext } from "react-icons/md";
+import { FaAngleLeft } from "react-icons/fa6";
+import CardServices from "@/Components/CardServices";
+import ModalServices from "@/Components/modalServices";
 import Tag from "@/Components/Tag";
-import {FaPlaceOfWorship} from 'react-icons/fa';
+import { useTranslations } from "use-intl";
 
-const Services : React.FC= ()=>{
-    const services=[
-        //     { title: t('place_1.title'), city:  t('place_1.city'), descr: t('place_1.descr'), image: '/places/Agadir/agadir-oufella.jpg' },
+interface Services{
+    
+    title: string;
+    desc: string;
+    image:string;
+    images: string[];// Tableau d'images
 
-        {title:"Transport and logistics",desc:"",img:{}},
-        {title:"Tours trip travel and adventures",desc:"",img:{}},
-        {title:"Sport and training programs and camps",desc:"",img:{}},
-        {title:"Shuttle and airport transfer",desc:"",img:{}},
-        {title:"Event and parties",desc:"",img:{}},
-        {title:"Accommodation and car rental",desc:"",img:{}},
+}
+const Services: React.FC = () => {
+  const services = [
+{
+      title: "Transport and logistics",desc: "Efficient solutions for moving goods globally.", image:"/Services/transport/img1.jpg",
+   images: ["/Services/transport/img1.jpg","/Services/transport/img2.jpg","/Services/transport/img3.jpg","/Services/transport/img4.jpg","/Services/transport/img5.jpg", ], 
+},
+{
+      title: "Accommodation and car rental",desc: "Efficient solutions for moving goods globally.", image:"/Services/accommodation/img1.jpg",
+   images: ["/Services/accommodation/img1.jpg","/Services/accommodation/img3.jpg","/Services/accommodation/img4.jpg","/Services/accommodation/img5.jpg","/Services/accommodation/img6.jpg","/Services/accommodation/img7.jpg","/Services/accommodation/img8.jpg","/Services/accommodation/img9.jpg","/Services/accommodation/img10.jpg","/Services/accommodation/img11.jpg","/Services/accommodation/img12.jpg" ], 
+},
+{
+      title: "Event and parties",desc: "Efficient solutions for moving goods globally.", image:"/Services/event/img1.jpg",
+   images: ["/Services/event/img1.jpg","/Services/event/img2.jpg","/Services/event/img3.jpg",], 
+},
+{
+      title: "Shuttle and airport transfer",desc: "Efficient solutions for moving goods globally.", image:"/Services/shuttle/img1.jpg",
+   images: ["/Services/shuttle/img1.jpg","/Services/shuttle/img2.jpg","/Services/shuttle/img3.jpg","/Services/shuttle/img4.jpg","/Services/shuttle/img6.jpg","/Services/shuttle/img7.jpg","/Services/shuttle/img5.jpg", ], 
+},
+{
+      title: "Sport and training programs and camps",desc: "Efficient solutions for moving goods globally.", image:"/Services/sport/img1.jpg",
+   images: ["/Services/sport/img1.jpg","/Services/sport/img2.jpg","/Services/sport/img3.jpg","/Services/sport/img4.jpg","/Services/sport/img5.jpg","/Services/sport/img6.jpg","/Services/sport/img7.jpg","/Services/sport/img8.jpg",
+           ], 
+},
+{
+      title: "Tours trip travel and adventures",desc: "Efficient solutions for moving goods globally.", image:"/Services/tours/img1.jpg",
+   images: ["/Services/tours/img1.jpg","/Services/tours/img2.jpg","/Services/tours/img3.jpg","/Services/tours/img4.jpg","/Services/tours/img5.jpg","/Services/tours/img6.jpg","/Services/tours/img7.jpg","/Services/tours/img8.jpg","/Services/tours/img9.jpg","/Services/tours/img10.jpg","/Services/tours/img11.jpg","/Services/tours/img12.jpg","/Services/tours/img13.jpg","/Services/tours/img15.jpg","/Services/tours/img15.jpg","/Services/tours/img16.jpg",
+           ], 
+          
+},
+             ]
 
-    ]
-   return(
+const t = useTranslations('homepage.services');
+  
+const [currentImage, setCurrentImage] = useState(0);
+const [isOpen, setIsOpen] = useState(false);
+const [modalData, setModalData] = useState<Services | null>(null);
+
+const openModal = (service:Services,index:number) => {
+    setModalData(service)
+    setCurrentImage(index);
+    setIsOpen(true);
+                 };
+                            
+const closeModal = () => setIsOpen(false);
+
+                             
+  return (
     <div>
+      <section id="services " >
+        <div className="text-center justify-center items-center">
+        <Tag icon={<FaPlaceOfWorship />}>{t('title')}</Tag>
 
-        <section id='services'> 
-            <div className="w-full px-10 py-10 flex flex-col items-center ">
-                <div className="text-center  flex justify-center py-8 w-full first-letter:"> 
-                <Tag icon={<FaPlaceOfWorship />}>Services</Tag>
-                    </div>
-
-                    <div className="     w-full lg:grid-cols-3 mg:grid-cols-4 grid-cols-1 grid  max-w-7xl gap-6">
-
-                        
-                {
-                    services.map((item,index)=>
-                        <div className=" cursor-pointer hover:bg-opacity-25">
-
-                            <ImageCard  title={item.title} desc={item.desc} img={item.img} />
-
-                        </div>
-
-                    )
-                }
-                    </div>
-
+        </div>
+        <div className="grid  lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 w-full max-w-7xl mx-auto px-4 py-8">
+          {services.map((service, index) => (
+            <div
+            onClick={() => openModal(service,0)}
+              key={index}
+              className="w-full h-full px-2 py-2 cursor-pointer  transition-shadow duration-300 ease-in-out"
+            >
+              <CardServices title={service.title} desc={service.desc} images={service.images}  image={service.image} />
             </div>
+          ))}
+        </div>
+      </section>
 
-            
-             </section>
+       {/* Modal */}
+            {modalData && (
+              <ModalServices isOpen={isOpen} onClose={closeModal} data={modalData} currentImage={currentImage}
+              setCurrentImage={setCurrentImage} />
+            )}
     </div>
-   )
-
+  );
 };
-export default Services ;
+
+export default Services;
