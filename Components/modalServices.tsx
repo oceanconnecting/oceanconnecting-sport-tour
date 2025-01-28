a8bf1ec1339ee1b1bc6fdfa3fa14c558de576fa2
ELBRIKIFATIMA <134740249+ELBRIKIFATIMA@users.noreply.github.com>
2025-01-28 14:28:11 +0100

    first

--- Components/modalServices.tsx
+++ Components/modalServices.tsx
@@ -36,6 +36,8 @@ const ModalServices: React.FC<ModalProps> = ({
     <AnimatePresence>
       {isOpen && (
         <motion.div
+
+        // commentaire 
           className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
