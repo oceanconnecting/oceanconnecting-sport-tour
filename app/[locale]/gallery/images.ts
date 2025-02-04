ff4dec79ced5e85474ad51d72ec28c0a69c4decf
Mohammed sadiki <firezettasadiki1024@gmail.com>
2025-02-04 12:13:05 +0100

    Refactor components and update styles; add new dependencies for lightbox functionality

--- app/[locale]/gallery/images.ts
+++ app/[locale]/gallery/images.ts
@@ -1,197 +1,197 @@
 
-const raw : string[] = Array.from({ length: 61 }, (_, i) => `/gallery/img/pic${i + 1}.jpg`);
+const raw = Array.from({ length: 61 }, (_, i) => ({ src: `/gallery/img/pic${i + 1}.jpg` }));
 
-export const Images = [
+export const Images : any = [
     {
         title: "some pictures",
-        images : raw
+        images: raw
     },
     {
         title: "agadir oufella",
         images: [
-            "/gallery/agadirOufella/agadirOufella1.jpg",
-            "/gallery/agadirOufella/agadirOufella2.jpg"
+            { src: "/gallery/agadirOufella/agadirOufella1.jpg" },
+            { src: "/gallery/agadirOufella/agadirOufella2.jpg" }
         ]
     },
     {
         title: "aqua park",
         images: [
-            "/gallery/aquaPark/aquaPark1.webp",
-            "/gallery/aquaPark/aquaPark2.webp",
-            "/gallery/aquaPark/aquaPark3.jpg",
-            "/gallery/aquaPark/aquaPark4.jpg"
+            { src: "/gallery/aquaPark/aquaPark1.webp" },
+            { src: "/gallery/aquaPark/aquaPark2.webp" },
+            { src: "/gallery/aquaPark/aquaPark3.jpg" },
+            { src: "/gallery/aquaPark/aquaPark4.jpg" }
         ]
     },
     {
         title: "camel ride",
         images: [
-            "/gallery/camelRide/camelRide1.webp",
-            "/gallery/camelRide/camelRide2.webp",
-            "/gallery/camelRide/camelRide3.jpg",
-            "/gallery/camelRide/camelRide4.jpg",
+            { src: "/gallery/camelRide/camelRide1.webp" },
+            { src: "/gallery/camelRide/camelRide2.webp" },
+            { src: "/gallery/camelRide/camelRide3.jpg" },
+            { src: "/gallery/camelRide/camelRide4.jpg" }
         ]
     },
     {
         title: "courniche",
         images: [
-            "/gallery/courniche/courniche1.webp",
-            "/gallery/courniche/courniche2.jpg",
+            { src: "/gallery/courniche/courniche1.webp" },
+            { src: "/gallery/courniche/courniche2.jpg" }
         ]
     },
     {
         title: "croco park",
         images: [
-            "/gallery/crocoPark/crocoPark1.jpg",
-            "/gallery/crocoPark/crocoPark2.jpeg",
+            { src: "/gallery/crocoPark/crocoPark1.jpg" },
+            { src: "/gallery/crocoPark/crocoPark2.jpeg" }
         ]
     },
     {
         title: "dolphin world",
         images: [
-            "/gallery/dolphinWorldAgadir/dolphinWorld1.jpg",
-            "/gallery/dolphinWorldAgadir/dolphinWorld2.jpg",
-            "/gallery/dolphinWorldAgadir/dolphinWorld3.jpg"
+            { src: "/gallery/dolphinWorldAgadir/dolphinWorld1.jpg" },
+            { src: "/gallery/dolphinWorldAgadir/dolphinWorld2.jpg" },
+            { src: "/gallery/dolphinWorldAgadir/dolphinWorld3.jpg" }
         ]
     },
     {
         title: "horse ride",
         images: [
-            "/gallery/horseRide/horseRide1.jpg",
-            "/gallery/horseRide/horseRide2.jpeg",
-            "/gallery/horseRide/horseRide3.jpeg",
-            "/gallery/horseRide/horseRide2.jpeg",
-            "/gallery/horseRide/horseRide2.jpg",
+            { src: "/gallery/horseRide/horseRide1.jpg" },
+            { src: "/gallery/horseRide/horseRide2.jpeg" },
+            { src: "/gallery/horseRide/horseRide3.jpeg" },
+            { src: "/gallery/horseRide/horseRide4.jpeg" },
+            { src: "/gallery/horseRide/horseRide5.jpg" }
         ]
     },
     {
         title: "hot air balloon",
         images: [
-            "/gallery/hotAirBalloon/hotAirBalloon1.jpg",
-            "/gallery/hotAirBalloon/hotAirBalloon2.jpeg",
-            "/gallery/hotAirBalloon/hotAirBalloon3.jpg"
+            { src: "/gallery/hotAirBalloon/hotAirBalloon1.jpg" },
+            { src: "/gallery/hotAirBalloon/hotAirBalloon2.jpeg" },
+            { src: "/gallery/hotAirBalloon/hotAirBalloon3.jpg" }
         ]
     },
     {
         title: "imi ouadar",
         images: [
-            "/gallery/imiOuadar/imiOuadar1.jpg"
+            { src: "/gallery/imiOuadar/imiOuadar1.jpg" }
         ]
     },
     {
         title: "jetski",
         images: [
-            "/gallery/jetski/jetski1.jpg",
-            "/gallery/jetski/jetski2.jpg",
+            { src: "/gallery/jetski/jetski1.jpg" },
+            { src: "/gallery/jetski/jetski2.jpg" }
         ]
     },
     {
         title: "karting",
         images: [
-            "/gallery/karting/karting1.png",
-            "/gallery/karting/karting2.jpg",
-            "/gallery/karting/karting3.jpg"
+            { src: "/gallery/karting/karting1.png" },
+            { src: "/gallery/karting/karting2.jpg" },
+            { src: "/gallery/karting/karting3.jpg" }
         ]
     },
     {
         title: "legzira",
         images: [
-            "/gallery/legzira/legzira1.jpg",
-            "/gallery/legzira/legzira2.jpg",
-            "/gallery/legzira/legzira3.jpg"
+            { src: "/gallery/legzira/legzira1.jpg" },
+            { src: "/gallery/legzira/legzira2.jpg" },
+            { src: "/gallery/legzira/legzira3.jpg" }
         ]
     },
     {
         title: "marina",
         images: [
-            "/gallery/marina/marina1.jpg",
-            "/gallery/marina/marina2.jpg",
-            "/gallery/marina/marina3.png"
+            { src: "/gallery/marina/marina1.jpg" },
+            { src: "/gallery/marina/marina2.jpg" },
+            { src: "/gallery/marina/marina3.png" }
         ]
     },
     {
         title: "medina and amazigh",
         images: [
-            "/gallery/medinaAndAmazighMuseum/medinaAmazigh1.jpg",
-            "/gallery/medinaAndAmazighMuseum/medinaAmazigh2.jpg",
-            "/gallery/medinaAndAmazighMuseum/medinaAmazigh3.jpeg"
+            { src: "/gallery/medinaAndAmazighMuseum/medinaAmazigh1.jpg" },
+            { src: "/gallery/medinaAndAmazighMuseum/medinaAmazigh2.jpg" },
+            { src: "/gallery/medinaAndAmazighMuseum/medinaAmazigh3.jpeg" }
         ]
     },
     {
         title: "parachute",
         images: [
-            "/gallery/parachute/parachute1.webp",
-            "/gallery/parachute/parachute2.jpg",
-            "/gallery/parachute/parachute3.jpeg",
-            "/gallery/parachute/parachute4.jpg",
-            "/gallery/parachute/parachute5.jpg",
+            { src: "/gallery/parachute/parachute1.webp" },
+            { src: "/gallery/parachute/parachute2.jpg" },
+            { src: "/gallery/parachute/parachute3.jpeg" },
+            { src: "/gallery/parachute/parachute4.jpg" },
+            { src: "/gallery/parachute/parachute5.jpg" }
         ]
     },
     {
         title: "paradise valley",
         images: [
-            "/gallery/paradiseValley/paradiseValley1.jpg",
-            "/gallery/paradiseValley/paradiseValley2.jpg",
-            "/gallery/paradiseValley/paradiseValley3.gif",
-            "/gallery/paradiseValley/paradiseValley4.jpg",
-            "/gallery/paradiseValley/paradiseValley5.jpg",
+            { src: "/gallery/paradiseValley/paradiseValley1.jpg" },
+            { src: "/gallery/paradiseValley/paradiseValley2.jpg" },
+            { src: "/gallery/paradiseValley/paradiseValley3.gif" },
+            { src: "/gallery/paradiseValley/paradiseValley4.jpg" },
+            { src: "/gallery/paradiseValley/paradiseValley5.jpg" }
         ]
     },
     {
         title: "quad n buggy",
         images: [
-            "/gallery/quadNBuggy/quadNBuggy1.jpg",
-            "/gallery/quadNBuggy/quadNBuggy2.jpg",
-            "/gallery/quadNBuggy/quadNBuggy3.jpg",
-            "/gallery/quadNBuggy/quadNBuggy4.jpg",
-            "/gallery/quadNBuggy/quadNBuggy5.jpg",
-            "/gallery/quadNBuggy/quadNBuggy6.jpg",
+            { src: "/gallery/quadNBuggy/quadNBuggy1.jpg" },
+            { src: "/gallery/quadNBuggy/quadNBuggy2.jpg" },
+            { src: "/gallery/quadNBuggy/quadNBuggy3.jpg" },
+            { src: "/gallery/quadNBuggy/quadNBuggy4.jpg" },
+            { src: "/gallery/quadNBuggy/quadNBuggy5.jpg" },
+            { src: "/gallery/quadNBuggy/quadNBuggy6.jpg" }
         ]
     },
     {
         title: "sandboarding",
         images: [
-            "/gallery/sandboarding/sandboarding1.jpg",
-            "/gallery/sandboarding/sandboarding2.jpg",
-            "/gallery/sandboarding/sandboarding3.jpg",
-            "/gallery/sandboarding/sandboarding4.jpg",
+            { src: "/gallery/sandboarding/sandboarding1.jpg" },
+            { src: "/gallery/sandboarding/sandboarding2.jpg" },
+            { src: "/gallery/sandboarding/sandboarding3.jpg" },
+            { src: "/gallery/sandboarding/sandboarding4.jpg" }
         ]
     },
     {
         title: "souk el had",
         images: [
-            "/gallery/soukElHad/soukElHad1.jpg",
-            "/gallery/soukElHad/soukElHad2.jpg",
-            "/gallery/soukElHad/soukElHad3.jpg"
+            { src: "/gallery/soukElHad/soukElHad1.jpg" },
+            { src: "/gallery/soukElHad/soukElHad2.jpg" },
+            { src: "/gallery/soukElHad/soukElHad3.jpg" }
         ]
     },
     {
         title: "sous massa",
         images: [
-            "/gallery/soussMassa/sousMassa1.jpg",
-            "/gallery/soussMassa/sousMassa2.png",
-            "/gallery/soussMassa/sousMassa3.jpg",
-            "/gallery/soussMassa/sousMassa4.jpg",
-            "/gallery/soussMassa/sousMassa5.jpg",
-            "/gallery/soussMassa/sousMassa6.jpg",
-            "/gallery/soussMassa/sousMassa7.jpg",
+            { src: "/gallery/soussMassa/sousMassa1.jpg" },
+            { src: "/gallery/soussMassa/sousMassa2.png" },
+            { src: "/gallery/soussMassa/sousMassa3.jpg" },
+            { src: "/gallery/soussMassa/sousMassa4.jpg" },
+            { src: "/gallery/soussMassa/sousMassa5.jpg" },
+            { src: "/gallery/soussMassa/sousMassa6.jpg" },
+            { src: "/gallery/soussMassa/sousMassa7.jpg" }
         ]
     },
     {
         title: "surfing",
         images: [
-            "/gallery/surfing/surfing1.jpg",
-            "/gallery/surfing/surfing2.jpg",
-            "/gallery/surfing/surfing3.png",
-            "/gallery/surfing/surfing4.jpg",
+            { src: "/gallery/surfing/surfing1.jpg" },
+            { src: "/gallery/surfing/surfing2.jpg" },
+            { src: "/gallery/surfing/surfing3.png" },
+            { src: "/gallery/surfing/surfing4.jpg" }
         ]
     },
     {
         title: "taghazout",
         images: [
-            "/gallery/taghazout/taghazout1.webp",
-            "/gallery/taghazout/taghazout2.jpg",
-            "/gallery/taghazout/taghazout3.jpg",
-            "/gallery/taghazout/taghazout4.jpg",
+            { src: "/gallery/taghazout/taghazout1.webp" },
+            { src: "/gallery/taghazout/taghazout2.jpg" },
+            { src: "/gallery/taghazout/taghazout3.jpg" },
+            { src: "/gallery/taghazout/taghazout4.jpg" }
         ]
     }
 ];
