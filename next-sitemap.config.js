9c06104ecbd6189bad09b899cfcd339f9eaa3612
EL BRIKI FATIMA <elbrikifatima19@gmail.com>
2025-03-14 12:31:42 +0000

    fd

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    siteUrl: 'https://www.oceantours.ma', // Your website's URL
-    generateRobotsTxt: true, // (Optional) Generates a robots.txt file
-    sitemapSize: 7000, // Number of URLs per sitemap file
-  }
